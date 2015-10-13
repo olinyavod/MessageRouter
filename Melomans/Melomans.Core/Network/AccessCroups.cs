baaeb195e64d3d1c376c98bde700868aff29afec
Кирилл Журавлёв <olinyavod@live.com>
2015-10-13 11:03:37 +1000

    Добвил атрибут группы доаступа на функции

---
+++ Melomans/Melomans.Core/Network/AccessCroups.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Melomans.Core.Network
+{
+	/// <summary>Группы функций</summary>
+	[DataContract]
+	public enum AccessCroups
+	{
+		/// <summary>Системный функции</summary>
+		[EnumMember]
+		System,
+
+		/// <summary>Управления воспроизвидением</summary>
+		[EnumMember]
+		ManipulatePlayer,
+
+		[EnumMember]
+		Player
+	}
+}
--- Melomans/Melomans.Core/Network/INetworkEventAgriggator.cs
+++ Melomans/Melomans.Core/Network/INetworkEventAgriggator.cs
@@ -6,11 +6,11 @@ namespace Melomans.Core.Network
 {
 	public interface INetworkEventAgriggator : IDisposable
 	{
-		IEnumerable<INetTask<TMessage>> Publish<TMessage>(TMessage message)
-			where TMessage : class, IMessage;
+		void Publish<TMessage>(TMessage message)
+			where TMessage : IMessage;
 
 		IEnumerable<INetTask<TMessage>> PublishFor<TMessage>(IEnumerable<Meloman> melomans, TMessage message)
-			where TMessage : class, IMessage;
+			where TMessage: IMessage;
 
 		IDisposable Subscribe<TMessage>(Action<INetTask<TMessage>> action)
 			where TMessage : class, IMessage;
