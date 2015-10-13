55d99ac1409b453580c92c0a826fe908bfdddcdb
Кирилл Журавлёв <olinyavod@live.com>
2015-10-14 09:09:09 +1000

    Добавил реализацию сериализатора сообщений

---
+++ Melomans/Melomans.Core/Network/IMessageSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Melomans.Core.Network
+{
+	public interface IMessageSerializer
+	{
+		Task WriteMessage<TMessage>(TMessage message, Stream stream);
+
+		Task<TMessage> ReadMessage<TMessage>(Stream stream);
+	}
+}
---
+++ Melomans/Melomans.Core/Network/JsonMessageSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Melomans.Core.Network
+{
+	public class JsonMessageSerializer : IMessageSerializer
+	{
+		public Task<TMessage> ReadMessage<TMessage>(Stream stream)
+		{
+			var serializer = new DataContractSerializer(typeof(TMessage));
+			return Task.Run(() => ((TMessage) serializer.ReadObject(stream)));
+		}
+
+		public Task WriteMessage<TMessage>(TMessage message, Stream stream)
+		{
+			var serializer = new DataContractJsonSerializer(typeof(TMessage));
+			return Task.Run(() => serializer.WriteObject(stream, message));
+		}
+	}
+}
