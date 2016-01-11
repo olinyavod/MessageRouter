0b8eb29f28e28c3f1b895dae8dcb1e2656da1d24
mesenev <heredemonsnearby@mail.ru>
2016-01-12 00:32:07 +1000

    network settings tabs fix

---
+++ Module.MessageRouter.Mobile/Network/NetworkSettings.cs
@@ -0,0 +1,33 @@
+﻿using Sockets.Plugin;
+using Sockets.Plugin.Abstractions;
+
+namespace Module.MessageRouter.Mobile.Network
+{
+	public class NetworkSettings
+	{
+		public int TTL {get;set;}
+
+		public int ListenPort  { get; set;}
+
+		public string MulticastAddress { get; set;}
+		public ICommsInterface Adapters { get; set; }
+		public int MulticastPort { get; set;}
+
+
+		public NetworkSettings ()
+		{
+			TTL = 10;
+			MulticastPort = 30307;
+			ListenPort = 30303;
+			MulticastAddress = "239.0.0.222";
+			Adapters = null;
+			var interfaces = Sockets.Plugin.CommsInterface.GetAllInterfacesAsync ().Result;
+			foreach (var i in interfaces) {
+				if (i.IsUsable && !i.IsLoopback) {
+					//Adapters = i;
+					break;
+				}			
+			}
+		}
+}
+}
