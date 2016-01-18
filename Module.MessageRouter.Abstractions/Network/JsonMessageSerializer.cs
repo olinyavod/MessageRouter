346d0dff96f5164adc4b1a24e0122859dfde78ca
mesenev <heredemonsnearby@mail.ru>
2016-01-18 22:36:16 +1000

    Changed default serializer to newtonsoft

--- Module.MessageRouter.Abstractions/Network/JsonMessageSerializer.cs
+++ Module.MessageRouter.Abstractions/Network/JsonMessageSerializer.cs
@@ -1,25 +1,31 @@
 ﻿using System.IO;
-using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Threading.Tasks;
 using Module.MessageRouter.Abstractions.Network.Interfaces;
+using Newtonsoft.Json;
 
 namespace Module.MessageRouter.Abstractions.Network
 {
 	public class JsonMessageSerializer : IMessageSerializer
 	{
-		public JsonMessageSerializer()
-		{ }
-
 		public async Task<TMessage> ReadMessage<TMessage>(Stream stream)
 		{
-			var serializer = new DataContractJsonSerializer(typeof(TMessage));
-		    return await Task.Run(() => ((TMessage) serializer.ReadObject(stream)));
+			using (var reader = new StreamReader(stream, Encoding.UTF8))
+			{
+				return await Task.Run(() => JsonConvert.DeserializeObject<TMessage>(reader.ReadToEnd()));
+			}
 		}
 
 		public Task WriteMessage<TMessage>(TMessage message, Stream stream)
 		{
-			var serializer = new DataContractJsonSerializer(typeof(TMessage));
-			return Task.Factory.StartNew(() => serializer.WriteObject(stream, message));
+			return Task.Run(() =>
+			{
+				var result = JsonConvert.SerializeObject(message);
+				using (var writer = new StreamWriter(stream, Encoding.UTF8))
+				{
+					writer.WriteLine(result);
+				}
+			});
 		}
 	}
 }
